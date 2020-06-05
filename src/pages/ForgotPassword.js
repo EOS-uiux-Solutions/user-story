77187a04323568d9c049598cbe424d9ce065afb6
dvijaywargiya <dvijaywargiya@gmail.com>
2020-06-05 14:41:38 +0530

    Added background image and reset.scss

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -23,52 +23,54 @@ export const ForgotPassword = () => {
   }
   const handleFormSubmit = (event) => {}
   return (
-    <div className='authentication'>
-      <div className='container-left'>
-        <div className='image image-logo'>
-          <img src={eosLogoWhite} alt='EOS Logo' />
-        </div>
-        <div className='image image-center'>
-          <img src={eosLock} alt='EOS Logo' />
-        </div>
-        <div>
-          <div className='header header-left'>Feature Request</div>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s
-          </p>
-        </div>
-        <div className='footer'>
-          This site saves some information in cookies but only when strictly
-          necessary
-          {/* <a href='#'>Learn More</a> */}
-        </div>
-      </div>
-      <div className='container-right'>
-        <div className='image image-logo eos-logo-resize'>
-          <img src={eosLogoColoured} alt='EOS Logo' />
-        </div>
-        <form className='form' onSubmit={handleFormSubmit}>
-          <div className='header'>Forgot Password</div>
-          <div className='form-group'>
-            <label htmlFor='email'>Email</label>
-            <input type='text' name='email' onChange={handleInputChange} />
+    <div className='authentication-wrapper'>
+      <div className='authentication'>
+        <div className='container-left'>
+          <div className='image image-logo'>
+            <img src={eosLogoWhite} alt='EOS Logo' />
           </div>
-          <Button
-            type='submit'
-            className='btn btn-default'
-            disabled={data.isSubmitting}
-          >
-            {data.isSubmitting ? 'Loading...' : 'Submit'}
-          </Button>
-        </form>
-        <Link className='link link-redirect' to='/'>
-          Existing User?
-        </Link>
-        {data.errorMessage && (
-          <span className='form-error'>{data.errorMessage}</span>
-        )}
+          <div className='image image-center'>
+            <img src={eosLock} alt='EOS Logo' />
+          </div>
+          <div>
+            <div className='header header-left'>Feature Request</div>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </p>
+          </div>
+          <div className='footer'>
+            This site saves some information in cookies but only when strictly
+            necessary
+            {/* <a href='#'>Learn More</a> */}
+          </div>
+        </div>
+        <div className='container-right'>
+          <div className='image image-logo eos-logo-resize'>
+            <img src={eosLogoColoured} alt='EOS Logo' />
+          </div>
+          <form className='form' onSubmit={handleFormSubmit}>
+            <div className='header'>Forgot Password</div>
+            <div className='form-group'>
+              <label htmlFor='email'>Email</label>
+              <input type='text' name='email' onChange={handleInputChange} />
+            </div>
+            <Button
+              type='submit'
+              className='btn btn-default'
+              disabled={data.isSubmitting}
+            >
+              {data.isSubmitting ? 'Loading...' : 'Submit'}
+            </Button>
+          </form>
+          <Link className='link link-redirect' to='/'>
+            Existing User?
+          </Link>
+          {data.errorMessage && (
+            <span className='form-error'>{data.errorMessage}</span>
+          )}
+        </div>
       </div>
     </div>
   )
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -26,75 +26,77 @@ export const Register = () => {
   const handleFormSubmit = (event) => {}
 
   return (
-    <div className='authentication'>
-      <div className='container-left'>
-        <div className='image image-logo'>
-          <img src={eosLogoWhite} alt='EOS Logo' />
-        </div>
-        <div className='image image-center'>
-          <img src={eosLock} alt='EOS Logo' />
-        </div>
-        <div>
-          <div className='header header-left'>Feature Request</div>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s
-          </p>
-        </div>
-        <div className='footer'>
-          This site saves some information in cookies but only when strictly
-          necessary
-          {/* <a href='#'>Learn More</a> */}
-        </div>
-      </div>
-      <div className='container-right'>
-        <div className='image image-logo eos-logo-resize'>
-          <img src={eosLogoColoured} alt='EOS Logo' />
-        </div>
-        <form className='form' onSubmit={handleFormSubmit}>
-          <div className='header'>Sign up</div>
-          <div className='form-group'>
-            <label htmlFor='username'>Username</label>
-            <input type='text' name='username' onChange={handleInputChange} />
+    <div className='authentication-wrapper'>
+      <div className='authentication'>
+        <div className='container-left'>
+          <div className='image image-logo'>
+            <img src={eosLogoWhite} alt='EOS Logo' />
           </div>
-          <div className='form-group'>
-            <label htmlFor='email'>Email</label>
-            <input type='text' name='email' onChange={handleInputChange} />
+          <div className='image image-center'>
+            <img src={eosLock} alt='EOS Logo' />
           </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Password</label>
-            <input
-              type='password'
-              name='password'
-              onChange={handleInputChange}
-            />
+          <div>
+            <div className='header header-left'>Feature Request</div>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </p>
           </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Confirm Password</label>
-            <input
-              type='password'
-              name='password'
-              onChange={handleInputChange}
-            />
+          <div className='footer'>
+            This site saves some information in cookies but only when strictly
+            necessary
+            {/* <a href='#'>Learn More</a> */}
           </div>
-          <Button
-            type='submit'
-            className='btn btn-default'
-            disabled={data.isSubmitting}
-          >
-            {data.isSubmitting ? 'Loading...' : 'Register'}
-          </Button>
-        </form>
-        <Link className='link link-redirect' to='/'>
-          Existing User?
-        </Link>
-        <div className='footer'>
-          <span> Copyright 2020 EOS </span>
         </div>
-        {data.errorMessage && (
-          <span className='form-error'>{data.errorMessage}</span>
-        )}
+        <div className='container-right'>
+          <div className='image image-logo eos-logo-resize'>
+            <img src={eosLogoColoured} alt='EOS Logo' />
+          </div>
+          <form className='form' onSubmit={handleFormSubmit}>
+            <div className='header'>Sign up</div>
+            <div className='form-group'>
+              <label htmlFor='username'>Username</label>
+              <input type='text' name='username' onChange={handleInputChange} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='email'>Email</label>
+              <input type='text' name='email' onChange={handleInputChange} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                name='password'
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='password'>Confirm Password</label>
+              <input
+                type='password'
+                name='password'
+                onChange={handleInputChange}
+              />
+            </div>
+            <Button
+              type='submit'
+              className='btn btn-default'
+              disabled={data.isSubmitting}
+            >
+              {data.isSubmitting ? 'Loading...' : 'Register'}
+            </Button>
+          </form>
+          <Link className='link link-redirect' to='/'>
+            Existing User?
+          </Link>
+          <div className='footer'>
+            <span> Copyright 2020 EOS </span>
+          </div>
+          {data.errorMessage && (
+            <span className='form-error'>{data.errorMessage}</span>
+          )}
+        </div>
       </div>
     </div>
   )
