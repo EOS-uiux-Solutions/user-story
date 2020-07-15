5b72d3393974d5c9cb0ecf91d7f96e1139682e76
dvijaywargiya <dvijaywargiya@gmail.com>
2020-07-15 20:33:04 +0530

    Resolved issues

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -7,52 +7,50 @@ const Comments = (props) => {
   const { comments } = props
 
   return (
-    <div className='comments-content'>
-      <div className='comments-wrapper'>
-        <h3>Comments</h3>
-        {comments.length ? (
-          comments.map((data, key) => {
-            return (
-              <div className='comment' key={key}>
-                <div className='user-avatar'>
-                  <img
-                    className='avatar'
-                    src={require('../assets/images/default-user.png')}
-                    alt='Default User Avatar'
-                  ></img>
+    <div className='comments-wrapper'>
+      <h3>Comments</h3>
+      {comments.length ? (
+        comments.map((data, key) => {
+          return (
+            <div className='comment' key={key}>
+              <div className='user-avatar'>
+                <img
+                  className='avatar'
+                  src={require('../assets/images/default-user.png')}
+                  alt='Default User Avatar'
+                ></img>
+              </div>
+              <div className='comment-content'>
+                <Link className='link link-default' to='#'>
+                  {data.user.username}
+                </Link>
+                <div className='metadata'>
+                  <div>
+                    {`${data.createdAt.slice(0, 10)}  ${data.createdAt.slice(
+                      11,
+                      19
+                    )}`}
+                  </div>
                 </div>
-                <div className='comment-content'>
+                <p>{data.Comments}</p>
+                <div className='reply-action'>
                   <Link className='link link-default' to='#'>
-                    {data.user.username}
+                    Reply
                   </Link>
-                  <div className='metadata'>
-                    <div>
-                      {`${data.createdAt.slice(0, 10)}  ${data.createdAt.slice(
-                        11,
-                        19
-                      )}`}
-                    </div>
-                  </div>
-                  <div className='text'>{data.Comments}</div>
-                  <div className='reply-action'>
-                    <Link className='link link-default' to='#'>
-                      Reply
-                    </Link>
-                  </div>
                 </div>
               </div>
-            )
-          })
-        ) : (
-          <h3>No comments yet</h3>
-        )}
-        <form className='comment-form'>
-          <div className='field'>
-            <textarea rows='4' cols='16'></textarea>
-          </div>
-          <Button className='btn btn-default'>Add Comment</Button>
-        </form>
-      </div>
+            </div>
+          )
+        })
+      ) : (
+        <h3>No comments yet</h3>
+      )}
+      <form className='comment-form'>
+        <div className='field'>
+          <textarea rows='4' cols='16'></textarea>
+        </div>
+        <Button className='btn btn-default'>Add Comment</Button>
+      </form>
     </div>
   )
 }
