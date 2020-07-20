701cfdc2e32164bcb24302b2fe35cc3463d6e7a0
devesh vijaywargiya <dvijaywargiya@gmail.com>
2020-07-20 15:36:32 +0000

    Added Product Dropdown on Homepage

--- src/components/StoriesList.js
+++ src/components/StoriesList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { navigate } from '@reach/router'
 
 const StoriesList = (props) => {
-  const { stories, state } = props
+  const { stories, state, product } = props
 
   const strip = (html) => {
     return html.replace(/<\s*[^>]*>/gi, '')
@@ -10,38 +10,10 @@ const StoriesList = (props) => {
 
   return (
     <>
-      {!state ? (
-        stories.length ? (
-          stories.map((story, key) => {
-            return (
-              <div
-                className='story'
-                key={key}
-                onClick={() => {
-                  navigate(`/story/${story.id}`)
-                }}
-              >
-                <div className='stories-content'>
-                  <h4>{story.Title}</h4>
-                  {strip(story.Description)}
-                </div>
-                <div className='icon-display'>
-                  {story.Votes}
-                  <i className='eos-icons'>thumb_up</i>
-                </div>
-                <div className='icon-display'>
-                  {story.user_story_comments.length}
-                  <i className='eos-icons'>comment</i>
-                </div>
-              </div>
-            )
-          })
-        ) : (
-          <h3>No stories</h3>
-        )
-      ) : stories.length ? (
+      {stories.length ? (
         stories.map((story, key) => {
-          return story.user_story_status.Status === state ? (
+          return story.user_story_status.Status === state &&
+            (story.product.Name === product || product === 'All') ? (
             <div
               className='story'
               key={key}
