28057b3a7c644f8778efbc85940dc6d3a84db851
dvijaywargiya <dvijaywargiya@gmail.com>
2020-08-21 15:52:10 +0530

    Updated Voting design

--- src/components/StoriesList.js
+++ src/components/StoriesList.js
@@ -17,22 +17,19 @@ const StoriesList = (props) => {
           return story.user_story_status.Status === state &&
             (story.product.Name === product || product === 'All') ? (
             <div className='story' key={key}>
+              <Vote story={story} />
               <div
+                className='stories-content'
                 onClick={() => {
                   navigate(`/story/${story.id}`)
                 }}
               >
-                <div className='stories-content'>
-                  <h4>{story.Title}</h4>
-                  {strip(story.Description)}
-                </div>
+                <h4>{story.Title}</h4>
+                {strip(story.Description)}
               </div>
-              <div>
-                <Vote story={story} />
-                <div className='icon-display'>
-                  {story.user_story_comments.length}
-                  <i className='eos-icons'>comment</i>
-                </div>
+              <div className='icon-display'>
+                {story.user_story_comments.length}
+                <i className='eos-icons'>comment</i>
               </div>
             </div>
           ) : (
