821a37e92ce09880bb1c70b056ff089dcb980fa3
Sorin <scurescu@suse.com>
2020-11-12 11:05:43 +0100

    Fix footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => {
   return (
     <div className='footer-main'>
-      <a className='link link-default' href='/polices'>
+      <a className='link link-default' href='/policies'>
         Cookies and Privacy policy
       </a>
 
