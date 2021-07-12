024461149819c7dfc9743b40b5b73f0be39988f5
Sundeep Chand <sundeep.chand2001@gmail.com>
2021-07-12 16:37:48 +0530

    Added timeout to comment

--- cypress/integration/user_story.spec.js
+++ cypress/integration/user_story.spec.js
@@ -110,7 +110,7 @@ describe('Test new User Registration Workflow', () => {
   it('Allows user to comment on a story', () => {
     cy.get('[data-cy=nav-eos-logo]').should('have.attr', 'href', '/').click()
 
-    cy.get('[data-cy=stories]').contains(testStory.title).click()
+    cy.get('[data-cy=stories]', { timeout: 1000 }).contains(testStory.title).click()
 
     cy.get('[data-cy=comment-input]').type(testComment)
 
