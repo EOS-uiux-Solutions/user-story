bac9db8872e75fe3abee6abcc16aee8e9314768b
Sundeep Chand <sundeep.chand2001@gmail.com>
2021-07-25 12:31:34 +0530

    Added skeleton loader

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { trackPromise, usePromiseTracker } from 'react-promise-tracker'
 import { Helmet } from 'react-helmet'
 import { navigate } from '@reach/router'
 
-import LoadingIndicator from '../modules/LoadingIndicator'
 import Stories from '../components/Stories'
 import Navigation from '../components/Navigation'
 import UserProfile from '../components/UserProfile'
@@ -14,15 +12,13 @@ const Profile = (props) => {
   const { profileId } = props
   const [user, setUser] = useState('')
 
-  const { promiseInProgress } = usePromiseTracker()
-
   useEffect(() => {
     const fetchUserInfo = async () => {
       const response = await userStory.getUserDetails(profileId)
       setUser(response.data.data.user)
     }
     if (profileId) {
-      trackPromise(fetchUserInfo())
+      fetchUserInfo()
     }
   }, [profileId])
 
@@ -41,15 +37,11 @@ const Profile = (props) => {
       <Navigation />
       <div className='body-content'>
         <div className='body-wrapper'>
-          {promiseInProgress ? (
-            <LoadingIndicator />
-          ) : (
-            <div className='flex flex-row flex-space-around'>
-              <div className='flex flex-column'>
-                <UserProfile user={user} />
-              </div>
+          <div className='flex flex-row flex-space-around'>
+            <div className='flex flex-column'>
+              <UserProfile user={user} />
             </div>
-          )}
+          </div>
           <div className='flex flex-column'>
             <h3>Stories by this user</h3>
             <Stories authorId={profileId} />
