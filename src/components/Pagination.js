7e843e25ed79377342e871f900e6e89d2b599c20
Ayush <ayushsatyam146@gmail.com>
2021-12-11 22:57:03 +0530

    removed unnecessary pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -30,56 +30,60 @@ const Pagination = (props) => {
     }
   }, [storyCount])
 
-  return (
-    <div className='pagination'>
-      <span
-        className={`btn btn-pagination ${
-          currNumber <= 1 ? 'btn-pagination-disabled' : ''
-        }`}
-        onClick={() => {
-          if (pages.find((page) => page === currNumber - 1)) {
-            setCurrNumber((currNumber) => currNumber - 1)
-            getPage(currNumber - 1)
-          }
-        }}
-      >
-        <EOS_KEYBOARD_ARROW_LEFT className='eos-icons eos-18' />
-        {`Prev`}
-      </span>
-      <div className='btn btn-pagination'>
-        {pages
-          ? pages.map((ele, key) => {
-              return (
-                <span
-                  className={`number ${currNumber === ele ? 'selected' : ''}`}
-                  onClick={() => {
-                    setCurrNumber(ele)
-                    getPage(ele)
-                  }}
-                  key={key}
-                >
-                  {ele}
-                </span>
-              )
-            })
-          : ''}
+  if (storyCount > 5) {
+    return (
+      <div className='pagination'>
+        <span
+          className={`btn btn-pagination ${
+            currNumber <= 1 ? 'btn-pagination-disabled' : ''
+          }`}
+          onClick={() => {
+            if (pages.find((page) => page === currNumber - 1)) {
+              setCurrNumber((currNumber) => currNumber - 1)
+              getPage(currNumber - 1)
+            }
+          }}
+        >
+          <EOS_KEYBOARD_ARROW_LEFT className='eos-icons eos-18' />
+          {`Prev`}
+        </span>
+        <div className='btn btn-pagination'>
+          {pages
+            ? pages.map((ele, key) => {
+                return (
+                  <span
+                    className={`number ${currNumber === ele ? 'selected' : ''}`}
+                    onClick={() => {
+                      setCurrNumber(ele)
+                      getPage(ele)
+                    }}
+                    key={key}
+                  >
+                    {ele}
+                  </span>
+                )
+              })
+            : ''}
+        </div>
+        <span
+          className={`btn btn-pagination ${
+            currNumber >= pages?.length ? 'btn-pagination-disabled' : ''
+          }`}
+          onClick={() => {
+            if (pages.find((page) => page === currNumber + 1)) {
+              setCurrNumber((currNumber) => currNumber + 1)
+              getPage(currNumber + 1)
+            }
+          }}
+        >
+          {`Next`}
+          <EOS_KEYBOARD_ARROW_RIGHT className='eos-icons eos-18' />
+        </span>
       </div>
-      <span
-        className={`btn btn-pagination ${
-          currNumber >= pages?.length ? 'btn-pagination-disabled' : ''
-        }`}
-        onClick={() => {
-          if (pages.find((page) => page === currNumber + 1)) {
-            setCurrNumber((currNumber) => currNumber + 1)
-            getPage(currNumber + 1)
-          }
-        }}
-      >
-        {`Next`}
-        <EOS_KEYBOARD_ARROW_RIGHT className='eos-icons eos-18' />
-      </span>
-    </div>
-  )
+    )
+  } else {
+    return null
+  }
 }
 
 export default Pagination
