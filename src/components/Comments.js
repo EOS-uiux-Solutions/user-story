b27a235fce907db2e163701b5b00c18736a2df1d
ayushsatyam146 <ayushsatyam146@gmail.com>
2021-12-14 11:44:19 +0530

    implemented Relative time(posted ago) in comments metadata

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -31,6 +31,23 @@ const toggleViewReplies = (viewRepliesToggled, setViewRepliesToggled, key) => {
       )
 }
 
+const intervals = [
+  { label: 'year', seconds: 31536000 },
+  { label: 'month', seconds: 2592000 },
+  { label: 'day', seconds: 86400 },
+  { label: 'hour', seconds: 3600 },
+  { label: 'minute', seconds: 60 },
+  { label: 'second', seconds: 1 }
+]
+
+function timeSince(creationDate) {
+  const date = new Date(creationDate)
+  const seconds = Math.floor((Date.now() - date.getTime()) / 1000)
+  const interval = intervals.find((i) => i.seconds < seconds)
+  const count = Math.floor(seconds / interval.seconds)
+  return `${count} ${interval.label}${count !== 1 ? 's' : ''} ago`
+}
+
 const Comments = (props) => {
   const { storyId } = props
 
@@ -141,12 +158,7 @@ const Comments = (props) => {
                   {data.user.username}
                 </Link>
                 <div className='metadata'>
-                  <div>
-                    {`${data.createdAt.slice(0, 10)}  ${data.createdAt.slice(
-                      11,
-                      19
-                    )}`}
-                  </div>
+                  <div>{timeSince(data.createdAt)}</div>
                 </div>
                 <div dangerouslySetInnerHTML={{ __html: data.Comments }} />
                 <div>
