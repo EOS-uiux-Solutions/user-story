9624bacbd818a4c20632a8219de273484ae3357e
kailash360 <kailashkejriwal21@gmail.com>
2022-01-10 16:06:04 +0530

    Added createdAt field in api call

--- src/services/user_story.js
+++ src/services/user_story.js
@@ -86,6 +86,7 @@ const userStory = {
                   username
                 }
                 Category
+                createdAt
               }
             }
             `
@@ -111,6 +112,7 @@ const userStory = {
             id
             url
           }
+          createdAt
         }
       }
       ${BASIC_STORY_INFO_FRAGMENT}
