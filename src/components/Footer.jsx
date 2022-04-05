b06aa4905bb73e149d9a8027ff0b67ffba6b1fa7
nishu-murmu <nishumurmu017@gmail.com>
2022-04-06 01:36:03 +0530

    (fix): removing cookies from footer adding it as banner.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-
+import CookieConsent from 'react-cookie-consent'
 function Footer() {
   return (
-    <div className='footer-main'>
-      <a className='link link-default' href='/policies'>
-        Cookies and Privacy policy
-      </a>
-
-      <p>We only use strictly necessary cookies</p>
-    </div>
+    <CookieConsent
+      debug={true}
+      location={'bottom'}
+      declineButtonStyle={{
+        backgroundColor: 'red',
+        padding: '10px',
+        borderRadius: '8px'
+      }}
+      buttonStyle={{
+        backgroundColor: 'grey',
+        padding: '10px',
+        borderRadius: '8px'
+      }}
+      declineButtonText={'I Decline'}
+      buttonText={'I Accept'}
+      style={{
+        background: 'white',
+        textAlign: 'left',
+        padding: '10px',
+        color: 'black'
+      }}
+      enableDeclineButton
+      flipButtons
+    >
+      <div className='footer-main'>
+        <a className='link link-default' href='/policies'>
+          Cookies and Privacy policy
+        </a>
+        <p>We only use strictly necessary cookies</p>
+      </div>
+    </CookieConsent>
   )
 }
 
