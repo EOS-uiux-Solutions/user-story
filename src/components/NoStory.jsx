f9b55ff4dd7895ad82acaf54d88d1be9d9d8b18d
kailash360 <kailashkejriwal21@gmail.com>
2022-06-29 12:21:43 +0530

    Fixed linting error

--- src/components/NoStory.jsx
+++ src/components/NoStory.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import '../assets/scss/components/NoStory.scss'
+import '../assets/scss/components/noStory.scss'
 
 function NoStory() {
   return (
-    <div className='noStory'>
+    <div className='no-story'>
       <p>No story found</p>
-      <a href='/newStory'>Create a new story instead</a>
+      <a href='/newStory' className='link'>
+        Create a new story instead
+      </a>
     </div>
   )
 }
