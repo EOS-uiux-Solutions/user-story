111623ae9c10a720079f5cb205a1b73fac348cba
Kailash Kejriwal <kailashkejriwal21@gmail.com>
2022-09-06 00:58:39 +0530

    created status container component

---
+++ src/components/StatusContainer.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { navigate, Link } from '@reach/router'
+import { EOS_COMMENT, EOS_ATTACHMENT } from 'eos-icons-react'
+import { strip } from '../utils/filterText'
+import Vote from './Vote'
+
+function StatusContainer(props) {
+  const { state, stories } = props
+
+  const filteredStories = stories.filter(
+    (story) => story.user_story_status.Status === state.status
+  )
+
+  console.log(state.status, filteredStories)
+
+  return (
+    <div className='status-container'>
+      <h3 className='status'>{state.status}</h3>
+      <div className='wrapper'>
+        {filteredStories.map((story, key) => (
+          <div className='story flex flex-column' key={key} draggable>
+            <div
+              data-cy='story-row'
+              className='stories-content'
+              onClick={() => {
+                navigate(`/story/${story.id}`)
+              }}
+            >
+              <h3>{strip(story.Title, 80)}</h3>
+              <p>{strip(story.Description, 80)}</p>
+            </div>
+            <div className='story-small-details flex flex-column'>
+              <div className='story-author story-subcontent flex'>
+                <div className='flex'>
+                  <small>Created By &nbsp;</small>
+                  <div className='user-avatar'>
+                    <img
+                      className='avatar'
+                      src={
+                        story?.author?.profilePicture?.url ??
+                        `https://avatars.dicebear.com/api/jdenticon/${story.author.username}.svg`
+                      }
+                      alt='Default User Avatar'
+                    ></img>
+                  </div>
+                  <Link
+                    className='link link-default'
+                    to={`/profile/${story.author.id}`}
+                  >
+                    {story.author.username}
+                  </Link>
+                </div>
+              </div>
+              <div className='flex story-subcontent'>
+                <small>Category &nbsp;</small>
+                <span className='category-text'>{story.Category}</span>
+              </div>
+              <div className='flex s-metas'>
+                <Vote story={story} small />
+                <span className='story-meta-wrapper'>
+                  <span className='story-meta'>
+                    <EOS_ATTACHMENT className='eos-icons' />
+                    {story.Attachment.length}
+                  </span>
+                  <span className='story-meta'>
+                    <EOS_COMMENT className='eos-icons' />
+                    {story.user_story_comments.length}
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default StatusContainer
