649f4e88206f3abafe953206cdb057eb202ae159
kunal00000 <kunalverma2468@gmail.com>
2023-02-05 13:31:02 +0530

    if description is empty it will not try and catch. directly show error.

--- src/pages/NewStory.js
+++ src/pages/NewStory.js
@@ -117,26 +117,26 @@ const NewStory = () => {
   }
 
   const onSubmit = async (data) => {
-    try {
-      data.Description = filterDescriptionText(description)
-      const formData = new FormData()
-      formData.append('data', JSON.stringify(data))
-      if (attachments.length) {
-        attachments.forEach((file) => {
-          formData.append('files.Attachment', file)
-        })
-      }
-      await userStory.createStory(formData)
-      toast.success('New story created successfully')
-      navigate('/')
-    } catch (err) {
-      console.error(err.data.message)
-      toast.error(err.data.message)
-      if (!description?.length) {
-        toast.error('Description cannot be empty')
-      } else {
+    if (description) {
+      try {
+        data.Description = filterDescriptionText(description)
+        const formData = new FormData()
+        formData.append('data', JSON.stringify(data))
+        if (attachments.length) {
+          attachments.forEach((file) => {
+            formData.append('files.Attachment', file)
+          })
+        }
+        await userStory.createStory(formData)
+        toast.success('New story created successfully')
+        navigate('/')
+      } catch (err) {
+        console.error(err.data.message)
+        toast.error(err.data.message)
         toast.error('Try to login again')
       }
+    } else {
+      toast.error('Description cannot be empty')
     }
   }
   return state.auth ? (
